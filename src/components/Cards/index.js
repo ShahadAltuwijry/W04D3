d66df66da11f9b96b3db3339b107b0a751b7d908
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-11-03 08:44:02 +0300

    updated

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -8,16 +8,19 @@ const concerts = [
     id: 0,
     name: "imagine dragons",
     img: "https://i.pinimg.com/564x/fe/e1/21/fee12160d1c7c03b8c175e6c81658fb0.jpg",
+    isFav: false,
   },
   {
     id: 1,
     name: "ColdPlay",
     img: "https://i.pinimg.com/564x/ed/0c/d1/ed0cd1fb68d9a31c12d5876e720bcca8.jpg",
+    isFav: true,
   },
   {
     id: 2,
     name: "Maroon 5",
     img: "https://i.pinimg.com/564x/36/4d/18/364d18abdb515f05fa649249e9ad8bc4.jpg",
+    isFav: false,
   },
 ];
 
@@ -29,6 +32,15 @@ const Cards = () => {
     history.push(`/CardDetails/${id}`);
   };
 
+  const addToFav = (id) => {
+    // concerts[id].isFav = !concerts[id].isFav;
+    // localStorage.setItem("concerts", JSON.stringify(concerts));
+  };
+
+  {
+    /*still didn't figure out how to do the favorite page */
+  }
+
   return (
     <div>
       {/*start of search function */}
@@ -39,12 +51,12 @@ const Cards = () => {
           setSearch(e.target.value);
         }}
       />
-            <h1>Cards</h1>
+      <h1>Cards</h1>
 
       <div className="searchDiv">
         {concerts
           .filter((val) => {
-            if (search == "") {
+            if (search === "") {
               return "no words written";
             } else if (
               val.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
@@ -64,13 +76,14 @@ const Cards = () => {
                   }}
                 />
                 <h2 className="name">{val.name}</h2>
+                <button className="favBtn" onClick={() => addToFav(concerts)}>
+                  {!concerts.isFav ? "❤" : "✘"}
+                </button>
               </div>
             );
           })}
       </div>
       {/*end of search function */}
-
-
     </div>
   );
 };
